1d05b9eac5e976a0d94d3af3114f6359014c983f
Jose De La Cruz <jldlcv@gmail.com>
2025-05-25 11:53:22 +0200

    Modifying settings file

--- djangomen/settings.py
+++ djangomen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t4327igkg6inql5ghfmu*&jw+ixzlsv=1o!s2s($6v_$1a9v^v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
