d66f040631c0f121cd05c4c46934a39caf1f5aee
Jose De La Cruz <jldlcv@gmail.com>
2025-06-01 09:25:06 +0200

    Edit form

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 # Create your views here.
 def post_list(request):
 	# Filtramos los posts que ya han sido publicados y los ordenamos en orden descendente
@@ -10,11 +11,13 @@ def post_list(request):
 	# Renderizamos la plantilla con los posts filtrados
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
 	# Obtenemos el post por su ID (pk) o devolvemos un error 404 si no existe
 	post = get_object_or_404(Post, pk=pk)
 	# Renderizamos la plantilla con el post obtenido
 	return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
 	# Si la petición es POST, significa que el usuario está enviando datos del formulario
@@ -35,3 +38,19 @@ def post_new(request):
 		form = PostForm()
 	# Renderizamos la plantilla con el formulario
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+	# Obtenemos el post por su ID (pk) o devolvemos un error 404 si no existe
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
