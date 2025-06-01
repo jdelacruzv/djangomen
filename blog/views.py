32698149c11c59f668522228c6fc21159ee14b86
Jose De La Cruz <jldlcv@gmail.com>
2025-06-01 14:54:12 +0200

    Delete post

--- blog/views.py
+++ blog/views.py
@@ -64,3 +64,17 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
 	# Renderizamos la plantilla con los posts filtrados
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+    return redirect('post_list')
