4a647dd4eed91e439eaec0995d3522ce8bf1f343
Jose De La Cruz <jldlcv@gmail.com>
2025-06-13 23:49:56 +0200

    Refactor login template: center form, remove main padding, and add login icon

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,8 @@ class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('title', 'text')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
